85603671f049fbd532213a827abf32885664f46c
Si Chang <sichang@yelp.com>
2015-01-23 17:22:44 -0800

    Add swagger related dependencies and some example api-docs.

--- tests/views/test_hello.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from schematizer.views import hello
-
-
-def test_with_arguments(dummy_request):
-    dummy_request.matchdict = {'name': 'Darwin'}
-    response = hello.hello(dummy_request)
-    assert response == {'message': 'Hello Darwin!'}
