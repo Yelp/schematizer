b5be04850d5158b0ca06d78ef000377f82dac432
William Cheng <wscheng@yelp.com>
2015-06-16 14:24:40 -0700

    formatted search on topics and schema

--- schematizer/static/js/app.js
+++ schematizer/static/js/app.js
@@ -1,23 +1,43 @@
 var app = angular.module('docToolApp', []);
 
-app.controller('schemaCtrl', ['$scope', '$http', function($scope, $http) {
-    $scope.schema = {};
-    $scope.showSchema = false;
+app.controller('docToolCtrl', ['$scope', '$http', function($scope, $http) {
+    $scope.data = [];
+    $scope.show = '';
     $scope.showSchemaError = false;
-    $scope.value = '';
+    $scope.text = '';
     $scope.control = 'schema';
+    $scope.showTopic = false;
 
-    $scope.searchByID = function() {
-        var path = '/v1/schemas/' + $scope.value;
+    $scope.search = function() {
+        switch ($scope.control) {
+            case 'schema':
+                $scope.searchSchema($scope.text);
+                break;
+            case 'topic':
+                $scope.searchTopic($scope.text);
+                break;
+            default: // Should never reach here
+        }
+    }
+
+    $scope.searchSchema = function(schemaID) {
+        var path = '/v1/schemas/' + schemaID;
         $http.get(path).success(function (data) {
-            $scope.schema.rawSchema = data;
-            $scope.schema.id = $scope.value
-            $scope.showSchema=true;
-            $scope.showSchemaError = false;
+            $scope.data = data;
+            $scope.show = 'schema';
         }).error(function (errorData) {
-            $scope.schema = {};
-            $scope.showSchema = false;
-            $scope.showSchemaError = true;
+            $scope.show = 'schemaError';
         });
     }
+
+    $scope.searchTopic = function(topicName) {
+        var path = '/v1/topics/' + topicName + '/schemas';
+        $http.get(path).success(function (data) {
+            $scope.data = data;
+            $scope.show = 'topic';
+        }).error(function (errorData) {
+            $scope.show = 'topicError';
+        });
+    }
+
 }]);
