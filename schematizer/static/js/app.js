8a432dacccde248e816b4c94dcbb49e57a2e9369
William Cheng <wscheng@yelp.com>
2015-06-15 17:23:52 -0700

    search for schemas by id

--- schematizer/static/js/app.js
+++ schematizer/static/js/app.js
@@ -1,6 +1,22 @@
 var app = angular.module('docToolApp', []);
 
-app.controller('schemaCtrl', function($scope) {
-    $scope.schema = {id: 1, rawSchema: '{"status": "RW", "created_at": "1969-12-31T16:00:03", "updated_at": "1969-12-31T16:00:03", "topic": {"updated_at": "1969-12-31T16:00:00", "source": {"created_at": "1969-12-31T16:00:01", "namespace": "yelp", "updated_at": "1969-12-31T16:00:01", "source_owner_email": "bam@yelp.com", "source": "business", "source_id": 1}, "created_at": "1969-12-31T16:00:00", "name": "yelp.business.v1", "topic_id": 2}, "schema_id": 1, "schema": "test schema"}'};
-    $scope.showSchema = 1;
-});
+app.controller('schemaCtrl', ['$scope', '$http', function($scope, $http) {
+    $scope.schema = {};
+    $scope.showSchema = false;
+    $scope.showSchemaError = false;
+    $scope.value = '';
+
+    $scope.searchByID = function() {
+        var path = '/v1/schemas/' + $scope.value;
+        $http.get(path).success(function (data) {
+            $scope.schema.rawSchema = data;
+            $scope.schema.id = $scope.value
+            $scope.showSchema=true;
+            $scope.showSchemaError = false;
+        }).error(function (errorData) {
+            $scope.schema = {};
+            $scope.showSchema = false;
+            $scope.showSchemaError = true;
+        });
+    }
+}]);
