bd488f2dc907dd5a47ed11721fe56c6d80fe9b5b
William Cheng <wscheng@yelp.com>
2015-06-25 10:26:45 -0700

    added created_at and updated_at columns for multiple tables

--- schema/tables/consumer_group_data_sources.sql
+++ schema/tables/consumer_group_data_sources.sql
@@ -3,5 +3,7 @@ CREATE TABLE `consumer_group_data_sources` (
   `consumer_group_id` int(11) NOT NULL,
   `data_source_type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `data_source_id` int(11) NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
--- schematizer/models/consumer_group.py
+++ schematizer/models/consumer_group.py
@@ -5,6 +5,7 @@ from sqlalchemy import Integer
 from sqlalchemy import String
 
 from schematizer.models.database import Base
+from schematizer.models.types.time import build_time_column
 
 
 class ConsumerGroup(Base):
@@ -16,5 +17,15 @@ class ConsumerGroup(Base):
     data_target_id = Column(
         Integer,
         ForeignKey('data_target.id'),
+        nullable=False
+    )
+
+    # Timestamp when the entry is created
+    created_at = build_time_column(default_now=True, nullable=False)
+
+    # Timestamp when the entry is last updated
+    updated_at = build_time_column(
+        default_now=True,
+        onupdate_now=True,
         nullable=False
     )
