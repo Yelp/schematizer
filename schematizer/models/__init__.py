43b8c89f38365647260defe2d02cf02891d8da5c
Chia-Chi Lin <clin@yelp.com>
2015-06-25 17:34:51 -0700

    add schema elements to allow access to inner avro schema components

--- schematizer/models/__init__.py
+++ schematizer/models/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # flake8: noqa
 from schematizer.models.avro_schema import *
+from schematizer.models.avro_schema_element import *
 from schematizer.models.consumer import *
 from schematizer.models.domain import *
 from schematizer.models.enums import *
