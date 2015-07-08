ddee16109cc4dca85bb01f71f2eed884783f5a30
William Cheng <wscheng@yelp.com>
2015-07-08 11:41:29 -0700

    note schema, models, and basic logic

---
+++ schematizer/logic/doc_tool.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+from schematizer import models
+
+from schematizer.models.database import session
+
+
+def get_note_by_schema_id(schema_id):
+    return session.query(
+        models.Note
+    ).join(
+        models.AvroSchema
+    ).filter(
+        models.Note.type == models.NoteTypeEnum.TABLE,
+        models.AvroSchema.id == models.Note.reference_id,
+    ).first()
+
+
+def get_note_by_schema_element_id(schema_element_id):
+    return session.query(
+        models.Note
+    ).join(
+        models.AvroSchemaElement
+    ).filter(
+        models.Note.type == models.NoteTypeEnum.FIELD,
+        models.AvroSchemaElement.id == models.Note.reference_id,
+    ).first()
+
+
+def create_or_update_table_note(schema_id, note_text, user_email):
+    note = get_note_by_schema_id(schema_id)
+    # update the note if one already exists, and return it
+    if note is not None:
+        _update_note(note.id, note_text, user_email)
+        return note
+    # create a new note if it does not exist
+    return _create_note(
+        models.NoteTypeEnum.TABLE,
+        schema_id,
+        note_text,
+        user_email
+    )
+
+
+def create_or_update_field_note(
+    schema_element_id,
+    note_text,
+    user_email
+):
+    note = get_note_by_schema_element_id(schema_element_id)
+    # update the note if one already exists, and return it
+    if note is not None:
+        _update_note(note.id, note_text, user_email)
+        return note
+    # create a new note if it does not exist
+    return _create_note(
+        models.NoteTypeEnum.FIELD,
+        schema_element_id,
+        note_text,
+        user_email
+    )
+
+
+def _update_note(note_id, note_text, user_email):
+    return session.query(
+        models.Note
+    ).filter(
+        models.Note.id == note_id
+    ).update(
+        {
+            models.Note.note: note_text,
+            models.Note.last_updated_by: user_email
+        },
+        synchronize_session='evaluate'
+    )
+
+
+def _create_note(note_type, reference_id, note_text, user_email):
+    note = models.Note(
+        note_type=note_type,
+        reference_id=reference_id,
+        note=note_text
+    )
+    session.add(note)
+    session.flush()
+    return note
---
+++ schematizer/models/note.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+from sqlalchemy import Column
+from sqlalchemy import Enum
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import Text
+from sqlalchemy import UniqueConstraint
+
+from schematizer.models.database import Base
+from schematizer.models.types.time import build_time_column
+
+
+class NoteTypeEnum(object):
+
+    # Table level note in the doc tool.
+    # Note references an object in the source table
+    TABLE = 'table'
+    # Field level note in the doc tool.
+    # Note references an object in the avro_schema_element table
+    FIELD = 'field'
+
+
+class Note(Base):
+
+    __tablename__ = 'source'
+    __table_args__ = (
+        UniqueConstraint(
+            'note_type',
+            'reference_id',
+            name='note_type_reference_id_unique_constraint'
+        ),
+    )
+
+    id = Column(Integer, primary_key=True)
+
+    # The type of object the note is referring to
+    note_type = Column(
+        Enum(
+            NoteTypeEnum.TABLE,
+            NoteTypeEnum.FIELD,
+            name='note_type_enum',
+        ),
+        nullable=False
+    )
+
+    # The id of the object the note is referring to.
+    # The db table for the id is specified by note_type
+    reference_id = Column(Integer, nullable=False)
+
+    # The note text
+    note = Column('note', Text)
+
+    # The email of the last user to update the note
+    last_updated_by = Column(String)
+
+    # Timestamp when the entry is created
+    created_at = build_time_column(
+        default_now=True,
+        nullable=False
+    )
+
+    # Timestamp when the entry is last updated
+    updated_at = build_time_column(
+        default_now=True,
+        onupdate_now=True,
+        nullable=False
+    )
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'type': self.note_type,
+            'reference_id': self.reference_id,
+            'note': self.note,
+            'last_updated_by': self.last_updated_by,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
