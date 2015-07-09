18425ac8b0a4fc72c9897d1ce3e04b725a32cf65
William Cheng <wscheng@yelp.com>
2015-07-09 14:28:55 -0700

    make note nullable

--- schema/tables/note.sql
+++ schema/tables/note.sql
@@ -2,7 +2,7 @@ CREATE TABLE `note` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `note_type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `reference_id` int(11) NOT NULL,
-  `note` text COLLATE utf8_unicode_ci NOT NULL,
+  `note` text COLLATE utf8_unicode_ci,
   `last_updated_by` varchar(255) COLLATE utf8_unicode_ci,
   `created_at` int(11) NOT NULL,
   `updated_at` int(11) NOT NULL,
