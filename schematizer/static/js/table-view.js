45e4879f1d317aeb98457f24df10c4c48cd74a44
William Cheng <wscheng@yelp.com>
2015-07-15 11:51:59 -0700

    http get for source works

--- schematizer/static/js/table-view.js
+++ schematizer/static/js/table-view.js
@@ -1,12 +1,29 @@
 (function() {
-    "use strict";
     var app = angular.module('tableView', []);
-
+    
+    "use strict";
     app.controller('TableViewController', ['$scope', '$http', '$location',
         function($scope, $http, $location){
 
             $scope.test = "This is a test string";
+            $scope.tableData = null;
+            $scope.load = true;
+            $scope.tableError = false;
             $scope.source_id = $location.search().id;
 
-        }])
+
+            function getSource() {
+                $http.get('/v1/sources/' + $scope.source_id).success(function (data) {
+                    $scope.tableData = data;
+                    $scope.load = false;
+                    $scope.tableError = false;
+                }).error(function (errorData) {
+                    $scope.sourcePromise = null;
+                    $scope.tableError = errorData;
+                });
+            };
+
+            getSource();
+
+        }]);
 })();
