84577eebd3e7b8f8cf2299e142ca1aa6fdfd69e6
William Cheng <wscheng@yelp.com>
2015-07-28 16:47:12 -0700

    fixed indexing for source_category

--- schema/tables/source_category.sql
+++ schema/tables/source_category.sql
@@ -5,6 +5,6 @@ CREATE TABLE `source_category` (
   `created_at` int(11) NOT NULL,
   `updated_at` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `source_id` (`source_id`),
-  UNIQUE KEY `category` (`category`)
+  UNIQUE KEY `source_id` (`source_id`),
+  KEY `category` (`category`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
