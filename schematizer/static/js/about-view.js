d47797470fc27f5ea3ac6273887c4bec2c6d8e9a
William Cheng <wscheng@yelp.com>
2015-07-28 17:08:55 -0700

    finished about page routing

---
+++ schematizer/static/js/about-view.js
@@ -0,0 +1,9 @@
+(function() {
+    "use strict";
+    var app = angular.module('about', []);
+
+    app.controller('AboutController', ['$scope', '$http',
+        function($scope, $http){
+
+        }])
+})();
--- schematizer/static/js/app.js
+++ schematizer/static/js/app.js
@@ -18,6 +18,10 @@
                 templateUrl: 'partials/table-view.html',
                 controller: 'TableViewController'
             }).
+            when('/about', {
+                templateUrl: 'partials/about.html',
+                controller: 'AboutController'
+            }).
             otherwise({
                 redirectTo: '/home'
             });
