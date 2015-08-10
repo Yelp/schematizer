c1cc48846165ac615a29f761d8f7ed9527f47fb9
William Cheng <wscheng@yelp.com>
2015-08-10 11:04:27 -0700

    set up browse page, finished filter box

--- schematizer/static/js/home.js
+++ schematizer/static/js/home.js
@@ -1,9 +1,32 @@
 (function() {
-    "use strict";
     var app = angular.module('home', []);
 
+    "use strict";
     app.controller('HomeController', ['$scope', '$http',
         function($scope, $http){
 
-        }])
+            $scope.tables = [];
+            $scope.load = true;
+            $scope.schemaFilter = 'public_v1';
+            $scope.allCategories = '[ All Categories ]';
+            $scope.uncategorized = '[ Uncategorized ]';
+            $scope.categoryFilter = $scope.allCategories;
+            $scope.categories = [];
+
+            function initBrowse() {
+                $http.get('/v1/namespaces/public_v1/sources').success(function (data) {
+                    $scope.tables = data;
+                    $scope.load = false;
+                }).error(function (errorData) {
+                    $scope.error = errorData;
+                    $scope.load = false;
+                });
+                $http.get('/v1/categories').success(function (data) {
+                    $scope.categories = data;
+                });
+            };
+
+            initBrowse();
+
+        }]);
 })();
