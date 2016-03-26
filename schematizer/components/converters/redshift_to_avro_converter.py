52ed2a6a4e9ecbe5a2e9865784b19406edd53bc4
Bo Wu <bowu@yelp.com>
2016-03-25 18:04:56 -0700

    save progress in converting sql statements to avro

---
+++ schematizer/components/converters/redshift_to_avro_converter.py
@@ -0,0 +1,285 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from yelp_avro.avro_builder import AvroSchemaBuilder
+from yelp_avro.data_pipeline.avro_meta_data import AvroMetaDataKeys
+
+from schematizer.components.converters.converter_base import BaseConverter
+from schematizer.components.converters.converter_base \
+    import SchemaConversionException
+from schematizer.components.converters.converter_base \
+    import UnsupportedTypeException
+from schematizer.models import redshift_data_types as redshift_types
+from schematizer.models import SchemaKindEnum
+from schematizer.models.sql_entities import MetaDataKey
+from schematizer.models.sql_entities import SQLTable
+
+
+class RedshiftToAvroConverter(BaseConverter):
+    """Converter that converts Redshift table schema to Avro schema.
+    """
+
+    source_type = SchemaKindEnum.Redshift
+    target_type = SchemaKindEnum.Avro
+
+    def __init__(self):
+        self._builder = AvroSchemaBuilder()
+
+    def convert(self, src_schema):
+        """The src_schema is the SQLTable object that represents a Redshift table.
+        It returns the Avro schema json object.
+        """
+        if not src_schema:
+            return None
+        if not isinstance(src_schema, SQLTable):
+            raise SchemaConversionException('SQLTable is expected.')
+
+        return self._create_avro_record_json(src_schema)
+
+    def _create_avro_record_json(self, table):
+        namespace = table.metadata.get(MetaDataKey.NAMESPACE, '')
+        aliases = table.metadata.get(MetaDataKey.ALIASES)
+        metadata = self._get_table_metadata(table)
+
+        self._builder.begin_record(
+            table.name,
+            namespace=namespace,
+            aliases=aliases,
+            doc=table.doc,
+            **metadata
+        )
+        for column in table.columns:
+            self._create_avro_field(column)
+        record_json = self._builder.end()
+        return record_json
+
+    def _get_table_metadata(self, table):
+        metadata = {}
+
+        primary_keys = [c.name for c in table.primary_keys]
+        if primary_keys:
+            metadata[AvroMetaDataKeys.PRIMARY_KEY] = primary_keys
+
+        return metadata
+
+    def _create_avro_field(self, column):
+        field_type, field_metadata = self._create_avro_field_type(column)
+        if column.is_nullable:
+            field_type = self._builder.begin_nullable_type(
+                field_type,
+                column.default_value
+            ).end()
+
+        self._builder.add_field(
+            column.name,
+            field_type,
+            has_default=column.default_value is not None or column.is_nullable,
+            default_value=column.default_value,
+            aliases=column.metadata.get(MetaDataKey.ALIASES),
+            doc=column.doc,
+            **field_metadata
+        )
+
+    def _create_avro_field_type(self, column):
+        type_cls = column.type.__class__
+        convert_func = self._type_converters.get(type_cls)
+        if convert_func:
+            return convert_func(column)
+
+        raise UnsupportedTypeException(
+            "Unable to convert redshift data type {} to Avro schema type."
+            .format(column.type)
+        )
+
+    @property
+    def _type_converters(self):
+        return {
+            redshift_types.RedshiftFloat4: self._convert_float_type,
+            redshift_types.RedshiftReal: self._convert_float_type,
+
+            redshift_types.RedshiftFloat: self._convert_double_type,
+            redshift_types.RedshiftDouble: self._convert_double_type,
+            redshift_types.RedshiftFloat8: self._convert_double_type,
+
+            redshift_types.RedshiftInt2: self._convert_small_integer_type,
+            redshift_types.RedshiftInt4: self._convert_small_integer_type,
+            redshift_types.RedshiftSmallInt: self._convert_small_integer_type,
+            redshift_types.RedshiftInteger: self._convert_small_integer_type,
+
+            redshift_types.RedshiftInt8: self._convert_bigint_type,
+            redshift_types.RedshiftBigInt: self._convert_bigint_type,
+
+            redshift_types.RedshiftNumeric: self._convert_decimal_type,
+            redshift_types.RedshiftDecimal: self._convert_decimal_type,
+
+            redshift_types.RedshiftBool: self._convert_boolean_type,
+            redshift_types.RedshiftBoolean: self._convert_boolean_type,
+
+            redshift_types.RedshiftNChar: self._convert_char_type,
+            redshift_types.RedshiftBPChar: self._convert_char_type,
+            redshift_types.RedshiftChar: self._convert_char_type,
+            redshift_types.RedshiftCharacter: self._convert_char_type,
+
+            redshift_types.RedshiftNVarChar: self._convert_varchar_type,
+            redshift_types.RedshiftCharacterVarying: self._convert_varchar_type,
+            redshift_types.RedshiftVarChar: self._convert_varchar_type,
+            redshift_types.RedshiftText: self._convert_string_type,
+
+            redshift_types.RedshiftDate: self._convert_date_type,
+            redshift_types.RedshiftDateAndTime: self._convert_datetime_type,
+            redshift_types.RedshiftTimestamp: self._convert_timestamp_type,
+        }
+
+    def _convert_small_integer_type(self, column):
+        metadata = self._get_primary_key_metadata(column.primary_key_order)
+        metadata.update(self._get_unsigned_metadata(column.type.is_unsigned))
+        return self._builder.create_int(), metadata
+
+    def _convert_integer_type(self, column):
+        # Avro int type is 4-byte signed integer. For MySQL unsigned int type,
+        # convert it to Avro long type, which is 8-type signed integer.
+        metadata = self._get_primary_key_metadata(column.primary_key_order)
+        metadata.update(self._get_unsigned_metadata(column.type.is_unsigned))
+        avro_type = (self._builder.create_long() if column.type.is_unsigned
+                     else self._builder.create_int())
+        return avro_type, metadata
+
+    def _get_primary_key_metadata(self, primary_key_order):
+        return ({AvroMetaDataKeys.PRIMARY_KEY: primary_key_order}
+                if primary_key_order else {})
+
+    def _get_unsigned_metadata(self, is_unsigned):
+        return ({AvroMetaDataKeys.UNSIGNED: is_unsigned}
+                if is_unsigned else {})
+
+    def _convert_bigint_type(self, column):
+        metadata = self._get_primary_key_metadata(column.primary_key_order)
+        metadata.update(self._get_unsigned_metadata(column.type.is_unsigned))
+        return self._builder.create_long(), metadata
+
+    def _convert_bit_type(self, column):
+        metadata = self._get_primary_key_metadata(column.primary_key_order)
+        metadata.update(self._get_bitlen_metadata(column.type.length))
+        return self._builder.create_int(), metadata
+
+    def _get_bitlen_metadata(self, bit_length):
+        return ({AvroMetaDataKeys.BIT_LEN: bit_length}
+                if bit_length else {})
+
+    def _convert_boolean_type(self, column):
+        metadata = self._get_primary_key_metadata(column.primary_key_order)
+        return self._builder.create_boolean(), metadata
+
+    def _convert_double_type(self, column):
+        metadata = self._get_primary_key_metadata(column.primary_key_order)
+        metadata.update(self._get_precision_metadata(column))
+        metadata.update(self._get_unsigned_metadata(column.type.is_unsigned))
+        return self._builder.create_double(), metadata
+
+    def _get_precision_metadata(self, column):
+        return {
+            AvroMetaDataKeys.PRECISION: column.type.precision,
+            AvroMetaDataKeys.SCALE: column.type.scale,
+        }
+
+    def _convert_float_type(self, column):
+        metadata = self._get_primary_key_metadata(column.primary_key_order)
+        metadata.update(self._get_precision_metadata(column))
+        metadata.update(self._get_unsigned_metadata(column.type.is_unsigned))
+        return self._builder.create_float(), metadata
+
+    def _convert_decimal_type(self, column):
+        metadata = self._get_primary_key_metadata(column.primary_key_order)
+        metadata.update(self._get_precision_metadata(column))
+        metadata.update(self._get_unsigned_metadata(column.type.is_unsigned))
+        metadata.update({AvroMetaDataKeys.FIXED_POINT: True})
+        return self._builder.create_double(), metadata
+
+    def _convert_string_type(self, column):
+        metadata = self._get_primary_key_metadata(column.primary_key_order)
+        return self._builder.create_string(), metadata
+
+    def _convert_char_type(self, column):
+        metadata = self._get_primary_key_metadata(column.primary_key_order)
+        metadata[AvroMetaDataKeys.FIX_LEN] = column.type.length
+        return self._builder.create_string(), metadata
+
+    def _convert_varchar_type(self, column):
+        metadata = self._get_primary_key_metadata(column.primary_key_order)
+        metadata[AvroMetaDataKeys.MAX_LEN] = column.type.length
+        return self._builder.create_string(), metadata
+
+    def _convert_date_type(self, column):
+        """Avro currently doesn't support date, so map the
+        date sql column type to string (ISO 8601 format)
+        """
+        metadata = self._get_primary_key_metadata(column.primary_key_order)
+        metadata[AvroMetaDataKeys.DATE] = True
+        return self._builder.create_string(), metadata
+
+    def _convert_datetime_type(self, column):
+        """Avro currently doesn't support datetime, so map the
+        datetime sql column type to string (ISO 8601 format)
+        """
+        metadata = self._get_primary_key_metadata(column.primary_key_order)
+        metadata[AvroMetaDataKeys.DATETIME] = True
+        return self._builder.create_string(), metadata
+
+    def _convert_time_type(self, column):
+        """Avro currently doesn't support time, so map the
+        time sql column type to string (ISO 8601 format)
+        """
+        metadata = self._get_primary_key_metadata(column.primary_key_order)
+        metadata[AvroMetaDataKeys.TIME] = True
+        return self._builder.create_string(), metadata
+
+    def _convert_year_type(self, column):
+        """Avro currently doesn't support year, so map the
+        year sql column type to long (year number)
+        """
+        metadata = self._get_primary_key_metadata(column.primary_key_order)
+        metadata[AvroMetaDataKeys.YEAR] = True
+        return self._builder.create_long(), metadata
+
+    def _convert_timestamp_type(self, column):
+        """Avro currently doesn't support timestamp, so map the
+        timestamp sql column type to long (unix timestamp)
+        """
+        metadata = self._get_primary_key_metadata(column.primary_key_order)
+        metadata[AvroMetaDataKeys.TIMESTAMP] = True
+        return self._builder.create_long(), metadata
+
+    def _convert_enum_type(self, column):
+        return self._builder.begin_enum(
+            self.get_enum_type_name(column),
+            column.type.values
+        ).end(), {}
+
+    def _convert_blob_type(self, column):
+        metadata = self._get_primary_key_metadata(column.primary_key_order)
+        return self._builder.create_bytes(), metadata
+
+    def _convert_binary_type(self, column):
+        metadata = self._get_primary_key_metadata(column.primary_key_order)
+        metadata[AvroMetaDataKeys.FIX_LEN] = column.type.length
+        return self._builder.create_bytes(), metadata
+
+    def _convert_varbinary_type(self, column):
+        metadata = self._get_primary_key_metadata(column.primary_key_order)
+        metadata[AvroMetaDataKeys.MAX_LEN] = column.type.length
+        return self._builder.create_bytes(), metadata
+
+    def _convert_set_type(self, column):
+        schema = self._builder.begin_array(
+            self._builder.begin_enum(
+                column.name,
+                column.type.values
+            ).end()
+        ).end()
+        metadata = self._get_primary_key_metadata(column.primary_key_order)
+        return schema, metadata
+
+    @classmethod
+    def get_enum_type_name(cls, column):
+        return column.name + '_enum'
---
+++ tests/components/converters/redshift_to_avro_converter_test.py
@@ -0,0 +1,316 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import pytest
+from yelp_avro.data_pipeline.avro_meta_data import AvroMetaDataKeys
+
+from schematizer.components.converters import RedshiftToAvroConverter
+from schematizer.components.converters.converter_base \
+    import SchemaConversionException
+from schematizer.components.converters.converter_base \
+    import UnsupportedTypeException
+from schematizer.models import redshift_data_types
+from schematizer.models.sql_entities import MetaDataKey
+from schematizer.models.sql_entities import SQLColumn
+from schematizer.models.sql_entities import SQLTable
+from testing.models.mysql_data_types import MySQLUnsupportedType
+
+
+class TestRedShiftToAvroConverter(object):
+
+    @pytest.fixture
+    def converter(self):
+        return RedshiftToAvroConverter()
+
+    @property
+    def table_name(self):
+        return 'foo'
+
+    @property
+    def empty_namespace(self):
+        return ''
+
+    @property
+    def namespace(self):
+        return 'ns'
+
+    def _convert_and_assert_with_one_column(self, converter,
+                                            sql_column, expected_field):
+        sql_table = SQLTable(self.table_name, [sql_column])
+        expected_schema = {
+            'type': 'record',
+            'name': self.table_name,
+            'namespace': self.empty_namespace,
+            'fields': [expected_field],
+        }
+        if sql_column.primary_key_order:
+            expected_schema.update(
+                {AvroMetaDataKeys.PRIMARY_KEY: [sql_column.name]}
+            )
+
+        actual_schema = converter.convert(sql_table)
+        assert expected_schema == actual_schema
+
+    def test_convert_with_col_int2(self, converter):
+        self._convert_and_assert_with_one_column(
+            converter,
+            SQLColumn('col_int2', redshift_data_types.RedshiftInt2()),
+            {'name': 'col_int2', 'type': ['null', 'int'], 'default': None}
+        )
+
+    def test_convert_with_col_int4(self, converter):
+        self._convert_and_assert_with_one_column(
+            converter,
+            SQLColumn('col_int4', redshift_data_types.RedshiftInt4()),
+            {'name': 'col_int4', 'type': ['null', 'int'], 'default': None}
+        )
+
+    def test_convert_with_col_int8(self, converter):
+        self._convert_and_assert_with_one_column(
+            converter,
+            SQLColumn('col_int8', redshift_data_types.RedshiftInt8()),
+            {'name': 'col_int8', 'type': ['null', 'long'], 'default': None}
+        )
+
+    def test_convert_with_col_integer(self, converter):
+        self._convert_and_assert_with_one_column(
+            converter,
+            SQLColumn('col_integer', redshift_data_types.RedshiftInteger()),
+            {'name': 'col_integer', 'type': ['null', 'int'], 'default': None}
+        )
+
+    def test_convert_with_col_bigint(self, converter):
+        self._convert_and_assert_with_one_column(
+            converter,
+            SQLColumn('col_bigint', redshift_data_types.RedshiftBigInt()),
+            {'name': 'col_bigint', 'type': ['null', 'long'], 'default': None}
+        )
+
+    def test_convert_with_col_smallint(self, converter):
+        self._convert_and_assert_with_one_column(
+            converter,
+            SQLColumn('col_smallint', redshift_data_types.RedshiftSmallInt()),
+            {'name': 'col_smallint', 'type': ['null', 'int'], 'default': None}
+        )
+
+    def test_convert_with_col_double(self, converter):
+        self._convert_and_assert_with_one_column(
+            converter,
+            SQLColumn('col_double', redshift_data_types.RedshiftDouble()),
+            {'name': 'col_double', 'type': ['null', 'double'], 'default': None}
+        )
+
+    def test_convert_with_col_decimal(self, converter):
+        self._convert_and_assert_with_one_column(
+            converter,
+            SQLColumn('col_decimal', redshift_data_types.RedshiftDecimal(8,0)),
+            {'name': 'col_decimal',
+             'type': ['null', 'double'],
+             'default': None,
+             AvroMetaDataKeys.PRECISION: 8,
+             AvroMetaDataKeys.SCALE: 0,
+             AvroMetaDataKeys.FIXED_POINT: True}
+        )
+
+    def test_convert_with_col_float4(self, converter):
+        self._convert_and_assert_with_one_column(
+            converter,
+            SQLColumn('col_float4', redshift_data_types.RedshiftFloat4() ),
+            {'name': 'col_float4', 'type': ['null', 'float'], 'default': None}
+        )
+
+    def test_convert_with_col_char(self, converter):
+        self._convert_and_assert_with_one_column(
+            converter,
+            SQLColumn('col_char', redshift_data_types.RedshiftChar(16)),
+            {'name': 'col_char',
+             'type': ['null', 'string'],
+             'default': None,
+             AvroMetaDataKeys.FIX_LEN: 16}
+        )
+
+    def test_convert_with_col_varchar(self, converter):
+        self._convert_and_assert_with_one_column(
+            converter,
+            SQLColumn('col_varchar', redshift_data_types.RedshiftVarChar(16)),
+            {'name': 'col_varchar',
+             'type': ['null', 'string'],
+             'default': None,
+             AvroMetaDataKeys.MAX_LEN: 16}
+        )
+
+    def test_convert_with_col_text(self, converter):
+        self._convert_and_assert_with_one_column(
+            converter,
+            SQLColumn('col_text', redshift_data_types.RedshiftText()),
+            {'name': 'col_text', 'type': ['null', 'string'], 'default': None},
+        )
+
+    def test_convert_with_col_date(self, converter):
+        self._convert_and_assert_with_one_column(
+            converter,
+            SQLColumn('col_date', redshift_data_types.RedshiftDate()),
+            {'name': 'col_date',
+             'type': ['null', 'string'],
+             'default': None,
+             AvroMetaDataKeys.DATE: True}
+        )
+
+    def test_convert_with_col_datetime(self, converter):
+        self._convert_and_assert_with_one_column(
+            converter,
+            SQLColumn('col_datetime', redshift_data_types.RedshiftDateAndTime()),
+            {'name': 'col_datetime',
+             'type': ['null', 'string'],
+             'default': None,
+             AvroMetaDataKeys.DATETIME: True}
+        )
+
+
+    def test_convert_with_col_timestamp(self, converter):
+        self._convert_and_assert_with_one_column(
+            converter,
+            SQLColumn('col_ts', redshift_data_types.RedshiftTimestamp()),
+            {'name': 'col_ts',
+             'type': ['null', 'long'],
+             'default': None,
+             AvroMetaDataKeys.TIMESTAMP: True}
+        )
+
+    def test_convert_with_col_bool(self, converter):
+        self._convert_and_assert_with_one_column(
+            converter,
+            SQLColumn('col_bool', redshift_data_types.RedshiftBool()),
+            {'name': 'col_bool', 'type': ['null', 'boolean'], 'default': None}
+        )
+
+    def test_convert_with_col_boolean(self, converter):
+        self._convert_and_assert_with_one_column(
+            converter,
+            SQLColumn('col_boolean',  redshift_data_types.RedshiftBoolean()),
+            {'name': 'col_boolean',
+             'type': ['null', 'boolean'],
+             'default': None}
+        )
+
+    def test_convert_with_unsupported_type(self, converter):
+        with pytest.raises(UnsupportedTypeException):
+            column = SQLColumn('col', MySQLUnsupportedType())
+            sql_table = SQLTable(self.table_name, [column])
+            converter.convert(sql_table)
+
+    def test_convert_with_primary_key_column(self, converter):
+        self._convert_and_assert_with_one_column(
+            converter,
+            SQLColumn(
+                'col',
+                redshift_data_types.RedshiftInteger(),
+                primary_key_order=1
+            ),
+            {'name': 'col',
+             'type': ['null', 'int'],
+             'default': None,
+             AvroMetaDataKeys.PRIMARY_KEY: True}
+        )
+
+    def test_convert_with_non_nullable_column(self, converter):
+        self._convert_and_assert_with_one_column(
+            converter,
+            SQLColumn(
+                'col',
+                redshift_data_types.RedshiftInteger(),
+                is_nullable=False,
+                default_value=0
+            ),
+            {'name': 'col', 'type': 'int', 'default': 0}
+        )
+
+    def test_convert_with_column_default_value(self, converter):
+        self._convert_and_assert_with_one_column(
+            converter,
+            SQLColumn('col', redshift_data_types.RedshiftInteger(), default_value=10),
+            {'name': 'col', 'type': ['int', 'null'], 'default': 10}
+        )
+
+    def test_convert_with_non_nullable_without_default_column(self, converter):
+        self._convert_and_assert_with_one_column(
+            converter,
+            SQLColumn('col', redshift_data_types.RedshiftInteger(), is_nullable=False),
+            {'name': 'col',
+             'type': 'int'}
+        )
+
+    def test_convert_with_none_table(self, converter):
+        actual_schema = converter.convert(None)
+        assert actual_schema is None
+
+    def test_convert_with_non_sqltable(self, converter):
+        with pytest.raises(SchemaConversionException):
+            converter.convert("create table")
+
+    def test_convert_with_table_namespace_and_aliases(self, converter):
+        doc = 'I am doc'
+        aliases = ['foo']
+        metadata = {
+            MetaDataKey.NAMESPACE: self.namespace,
+            MetaDataKey.ALIASES: aliases
+        }
+        col_name = 'col'
+        sql_table = SQLTable(
+            self.table_name,
+            [SQLColumn(col_name, redshift_data_types.RedshiftInteger())],
+            doc=doc,
+            **metadata
+        )
+
+        expected_schema = {
+            'type': 'record',
+            'name': self.table_name,
+            'namespace': self.namespace,
+            'fields': [
+                {'name': col_name, 'type': ['null', 'int'], 'default': None}
+            ],
+            'doc': doc,
+            'aliases': aliases
+        }
+
+        actual_schema = converter.convert(sql_table)
+        assert expected_schema == actual_schema
+
+    def test_convert_with_table_metadata(self, converter):
+        pkey_col1 = SQLColumn(
+            'pkey_col_one',
+            redshift_data_types.RedshiftInteger(),
+            primary_key_order=1
+        )
+        pkey_col2 = SQLColumn(
+            'pkey_col_two',
+            redshift_data_types.RedshiftInteger(),
+            primary_key_order=2
+        )
+        col = SQLColumn('col', redshift_data_types.RedshiftInteger())
+        sql_table = SQLTable(self.table_name, [pkey_col2, pkey_col1, col])
+
+        expected_schema = {
+            'type': 'record',
+            'name': self.table_name,
+            'namespace': '',
+            'fields': [
+                {'name': pkey_col2.name,
+                 'type': ['null', 'int'],
+                 'default': None,
+                 AvroMetaDataKeys.PRIMARY_KEY: 2},
+                {'name': pkey_col1.name,
+                 'type': ['null', 'int'],
+                 'default': None,
+                 AvroMetaDataKeys.PRIMARY_KEY: 1},
+                {'name': col.name,
+                 'type': ['null', 'int'],
+                 'default': None},
+            ],
+            AvroMetaDataKeys.PRIMARY_KEY: [pkey_col1.name, pkey_col2.name]
+        }
+
+        actual_schema = converter.convert(sql_table)
+        assert expected_schema == actual_schema
