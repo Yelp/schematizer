8aeb3df5379483b13779f156db2eff9ff58a6cf7
Bo Wu <bowu@dev12-devc.dev.yelpcorp.com>
2016-07-25 16:49:15 -0700

    finishing address code reviews

--- schematizer/logic/registration_repository.py
+++ schematizer/logic/registration_repository.py
@@ -7,7 +7,6 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 from sqlalchemy import exc
-from sqlalchemy import or_
 
 from schematizer import models
 from schematizer.logic.validators import verify_entity_exists
@@ -211,29 +210,30 @@ def get_data_targets_by_schema_id(schema_id):
                 models.ConsumerGroupDataSource.data_source_id == namespace_id
             ) &
             (
-                models.ConsumerGroupDataSource.data_source_type ==
+                models.ConsumerGroupDataSource.data_source_type == \
                 SrcType.NAMESPACE
             )
-        ) |
+        )|
         (
             (
                 models.ConsumerGroupDataSource.data_source_id == schema_id
             ) &
             (
-                models.ConsumerGroupDataSource.data_source_type ==
+                models.ConsumerGroupDataSource.data_source_type == \
                 SrcType.SCHEMA
             )
-        ) |
+        )|
         (
             (
                 models.ConsumerGroupDataSource.data_source_id == src_id
             ) &
             (
-                models.ConsumerGroupDataSource.data_source_type ==
+                models.ConsumerGroupDataSource.data_source_type == \
                 SrcType.SOURCE
             )
         )
     )
+
 
     consumer_group_ids.update(
         consumer_group.consumer_group_id for consumer_group in results
