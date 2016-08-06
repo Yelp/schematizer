21585988ebb6b5c7ef716b51b3316e3c6018efe5
Bo Wu <bowu@yelp.com>
2016-08-05 17:42:07 -0700

    fix bug in assertion

--- schematizer/views/schemas.py
+++ schematizer/views/schemas.py
@@ -16,11 +16,9 @@ from schematizer.config import get_config
 from schematizer.config import log
 from schematizer.logic import registration_repository as reg_repo
 from schematizer.logic import schema_repository
+from schematizer.models.exceptions import EntityNotFoundError
 from schematizer.utils.utils import get_current_func_arg_name_values
 from schematizer.views import view_common
-from schematizer.models.exceptions import EntityNotFoundError
-
-
 
 
 @view_config(
