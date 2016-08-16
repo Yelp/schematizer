f4a94632996d075f4962dd843552baaaccbe822b
Chia-Chi Lin <clin@yelp.com>
2016-08-16 00:51:02 -0700

    changed the endpoint for checking availability back to /v1/namespaces

--- schematizer/tools/register_tables.py
+++ schematizer/tools/register_tables.py
@@ -197,7 +197,7 @@ def _ensure_schematizer_is_ready(schematizer_host):
     while time.time() < timed_out:
         try:
             response = requests.get(
-                url='http://{}:8888/status'.format(schematizer_host),
+                url='http://{}:8888/v1/namespaces'.format(schematizer_host),
             )
             if response.status_code == 200:
                 return
