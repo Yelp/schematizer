daa6f727b9beca0a92df13c68131218e61dfd695
Tajinder Singh <tajinder@yelp.com>
2016-08-25 16:48:57 -0700

    updated liquibase xml file and sql file to add a new column 'alias' to avro_schema table

--- schema/tables/avro_schema.sql
+++ schema/tables/avro_schema.sql
@@ -3,6 +3,7 @@ CREATE TABLE `avro_schema` (
   `avro_schema` text COLLATE utf8_unicode_ci NOT NULL,
   `topic_id` int(11) NOT NULL,
   `base_schema_id` int(11) DEFAULT NULL,
+  `alias` varchar(255) DEFAULT NULL,
   `status` varchar(40) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'RW',
   `created_at` int(11) NOT NULL,
   `updated_at` int(11) NOT NULL,
