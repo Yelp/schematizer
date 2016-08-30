ecbd81ecd0b9a1160aa4862c7af95f3f94d6e54b
Tajinder Singh <tajinder@yelp.com>
2016-08-30 00:35:02 -0700

    1. updated liquibase xml file and sql file to add single index on 'alias' column in avro_schema table 2. updated code as per review comments.

--- schema/tables/avro_schema.sql
+++ schema/tables/avro_schema.sql
@@ -8,5 +8,6 @@ CREATE TABLE `avro_schema` (
   `created_at` int(11) NOT NULL,
   `updated_at` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `topic_id` (`topic_id`)
+  KEY `topic_id` (`topic_id`),
+  KEY `alias` (`alias`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
