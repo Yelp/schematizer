96792d8832507c4e4277ae298b4d7e3f0fa2654a
Abrar Sheikh <abrar@yelp.com>
2016-09-14 11:43:16 -0700

    convert memoized from class to function

--- schematizer/helpers/decorators.py
+++ schematizer/helpers/decorators.py
@@ -2,12 +2,11 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-import functools
+import cPickle
+from functools import wraps
 
-from six.moves import cPickle as pickle
 
-
-class memoized(object):
+def memoized(func):
     """Decorator that caches a function's return value each time it is called.
     If called later with the same arguments, the cached value is returned, and
     the function is not re-evaluated.
@@ -17,25 +16,12 @@ class memoized(object):
     For a memoization decorator with limited cache size, consider:
     http://code.activestate.com/recipes/496879-memoize-decorator-function-with-cache-size-limit/
     """
+    cache = {}
 
-    def __init__(self, func):
-        self.func = func
-        self.cache = {}
-
-    def __call__(self, *args, **kwargs):
-        # If the function args cannot be used as a cache hash key, fail fast
-        key = pickle.dumps((args, kwargs))
-        try:
-            return self.cache[key]
-        except KeyError:
-            value = self.func(*args, **kwargs)
-            self.cache[key] = value
-            return value
-
-    def __repr__(self):
-        """Return the function's docstring."""
-        return self.func.__doc__
-
-    def __get__(self, obj, objtype):
-        """Support instance methods."""
-        return functools.partial(self.__call__, obj)
+    @wraps(func)
+    def func_wrapper(*args, **kwargs):
+        key = cPickle.dumps((args, kwargs))
+        if key not in cache:
+            cache[key] = func(*args, **kwargs)
+        return cache[key]
+    return func_wrapper
