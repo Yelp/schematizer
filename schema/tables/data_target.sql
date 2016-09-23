6d0b6d83cf8b2077e8273a5084a4a7f690d66483
Tajinder Singh <tajinder@yelp.com>
2016-09-23 16:41:08 -0700

    add a nullable column 'name' to the data_target table.

--- schema/tables/data_target.sql
+++ schema/tables/data_target.sql
@@ -1,10 +1,9 @@
 CREATE TABLE `data_target` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
+  `name` varchar(255) NULL,
   `target_type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `destination` varchar(2048) COLLATE utf8_unicode_ci NOT NULL,
   `created_at` int(11) NOT NULL,
   `updated_at` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `name_unique_constraint` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
