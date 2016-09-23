0fa3b88c4f435e89858356930e5d770cecb5ed45
Tajinder Singh <tajinder@yelp.com>
2016-09-23 16:44:44 -0700

    reverted the sql file changes

--- schema/tables/data_target.sql
+++ schema/tables/data_target.sql
@@ -1,6 +1,5 @@
 CREATE TABLE `data_target` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NULL,
   `target_type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `destination` varchar(2048) COLLATE utf8_unicode_ci NOT NULL,
   `created_at` int(11) NOT NULL,
